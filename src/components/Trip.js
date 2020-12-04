333d0a55953e963a6a3bfc297350868eeeb9c102
Carlos Kruger <carloskr@yahoo.com>
2020-12-04 17:30:43 -0500

    added ability to add pictures and notes at the front end

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -8,11 +8,23 @@ class Trip extends Component {
 	constructor() {
 		super();
 		this.state = {
+			id: null,
 			trip: {},
 			notes: [],
 			pictures: [],
+			pic_title: "",
+			picture: "",
+			note_title: "",
+			note: "",
 		};
+		this.changeHandler = this.changeHandler.bind(this);
 	}
+
+	changeHandler = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value,
+		});
+	};
 
 	async componentDidMount() {
 		try {
@@ -27,6 +39,7 @@ class Trip extends Component {
 			console.log("notes: ", notes.data);
 			console.log("pictures: ", pictures.data);
 			this.setState({
+				id: id,
 				trip: trip.data,
 				notes: notes.data,
 				pictures: pictures.data,
@@ -35,11 +48,40 @@ class Trip extends Component {
 			console.log(err);
 		}
 	}
-	render() {
-		{
-			console.log("this.state.trip", this.state.trip);
-		}
 
+	addPicture = async (e) => {
+		e.preventDefault();
+
+		const { pic_title, picture, id } = this.state;
+		const title = pic_title;
+		console.log(pic_title);
+		try {
+			const newPicture = await axios.post(
+				`/api/trips/${this.state.id}/pictures`,
+				{ title, picture, id }
+			);
+		} catch (err) {
+			alert(err);
+		}
+	};
+
+	addNote = async (e) => {
+		e.preventDefault();
+
+		const { note_title, note, id } = this.state;
+		const title = note_title;
+		console.log(note_title);
+		try {
+			const newNote = await axios.post(`/api/trips/${this.state.id}/notes`, {
+				title,
+				note,
+				id,
+			});
+		} catch (err) {
+			alert(err);
+		}
+	};
+	render() {
 		return (
 			<div>
 				<div>
@@ -47,6 +89,74 @@ class Trip extends Component {
 					<p>Departure date: {this.state.trip.departure_date}</p>
 					<p>Return date: {this.state.trip.return_date} </p>
 				</div>
+
+				<div className="addPictureNote">
+					<div className="formBox">
+						<h4>Add picture</h4>
+						<input
+							name="pic_title"
+							type="text"
+							value={this.state.pic_title}
+							placeholder="title"
+							onChange={(e) => this.changeHandler(e)}
+						/>
+						<input
+							name="picture"
+							type="text"
+							value={this.state.picture}
+							placeholder="picture URL"
+							onChange={(e) => this.changeHandler(e)}
+						/>
+						<button onClick={(e) => this.addPicture(e)}>Add Picture</button>
+					</div>
+					<div className="formBox">
+						<h4>Add a Note</h4>
+						<input
+							name="note_title"
+							type="text"
+							value={this.state.note_title}
+							placeholder="title"
+							onChange={(e) => this.changeHandler(e)}
+						/>
+						<input
+							name="note"
+							type="text"
+							value={this.state.note}
+							placeholder="Enter your note"
+							onChange={(e) => this.changeHandler(e)}
+						/>
+						<button onClick={(e) => this.addNote(e)}>Add Note</button>
+					</div>
+				</div>
+				{this.state.pictures.map((picture, index) => (
+					<div className="tripbox" key={index}>
+						<div>
+							<span>
+								<b>{picture.title}</b>
+							</span>
+						</div>
+						<div>
+							<img
+								src={picture.picture}
+								alt={picture.title}
+								width="30%"
+								height="30%"
+							/>
+						</div>
+					</div>
+				))}
+				{this.state.notes.map((note, index) => (
+					<div className="tripbox" key={index}>
+						<div>
+							<span>
+								<b>{note.title}</b>
+							</span>
+						</div>
+						<div>
+							<p>{note.note} </p>
+						</div>
+					</div>
+				))}
 			</div>
 		);
 	}
