751d4341edbdb8148fd74f0207d081cb2e7b5053
Carlos Kruger <carloskr@yahoo.com>
2020-12-11 16:12:23 -0500

    added a header

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import routes from "./routes";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import Nav from "./components/Nav";
+import Header from "./components/Header";
 import React, { Component } from "react";
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				<Header />
 				{this.props.location.pathname !== "/" &&
 					this.props.location.pathname !== "/register" && <Nav />}
+
 				{routes}
 			</div>
 		);
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,90 +1,74 @@
-import React, {Component } from 'react';
-import axios from 'axios';
-import { connect } from 'react-redux';
-import {loginUser} from '../ducks/reducer';
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import axios from "axios";
+import { connect } from "react-redux";
+import { loginUser } from "../ducks/reducer";
+import { Link } from "react-router-dom";
 
 class Login extends Component {
-    constructor(){
-        super();
-        this.state = {
-            username: '',
-            password: '',
-           
-        }
-        this.changeHandler = this.changeHandler.bind(this)
-    }
+	constructor() {
+		super();
+		this.state = {
+			username: "",
+			password: "",
+		};
+		this.changeHandler = this.changeHandler.bind(this);
+	}
 
-    changeHandler = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
+	changeHandler = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value,
+		});
+	};
 
-    login = async (e) => {
-        e.preventDefault();
-        const {username, password} = this.state
-        try {
-            const user = await axios.post('/api/login', {username, password})
-      
-            this.props.loginUser(user.data)
-            this.props.history.push('/trips/search')
-        } 
-        catch(err){ 
-                alert(err)
-        }
-    }
+	login = async (e) => {
+		e.preventDefault();
+		const { username, password } = this.state;
+		try {
+			const user = await axios.post("/api/login", { username, password });
 
+			this.props.loginUser(user.data);
+			this.props.history.push("/trips/search");
+		} catch (err) {
+			alert(err);
+		}
+	};
 
+	render() {
+		const { username, password } = this.state;
+		return (
+			<div>
+				<br></br>
+				<br></br>
+				<div className="formBox">
+					<br></br>
+					<input
+						name="username"
+						type="username"
+						value={username}
+						placeholder="username"
+						onChange={(e) => this.changeHandler(e)}
+					/>
+					<input
+						name="password"
+						type="password"
+						value={password}
+						placeholder="password"
+						onChange={(e) => this.changeHandler(e)}
+					/>
+					<button onClick={(e) => this.login(e)}>Login</button>
 
-render(){
-
-
-    
-        
-        const {username, password} = this.state;
-        return(
-            <div className="formBox">
-            <input name="username" 
-                        type="username"
-                        value={ username }
-                        placeholder="username" 
-                        onChange={ e => this.changeHandler(e)}/>
-            <input    name="password" 
-                        type="password"
-                        value={password} 
-                        placeholder="password" 
-                        onChange={ e => this.changeHandler(e)}/>
-            <button onClick={e => this.login(e)}>Login</button>
-            
-            <button><Link to="/register" style={{ textDecoration: 'none' }}> Register</Link></button>
-            </div>
-        )
-    
-
+					<button>
+						<Link to="/register" style={{ textDecoration: "none" }}>
+							{" "}
+							Register
+						</Link>
+					</button>
+				</div>
+			</div>
+		);
+	}
 }
 
+const mapStateToProps = (state) => state;
 
-
-
-
-}
-
-
-
-
-
-
-
-const mapStateToProps = state => state
-
-export default connect(mapStateToProps, {loginUser})(Login)
-
-
-   
- 
-
-
-  
-     
-      
+export default connect(mapStateToProps, { loginUser })(Login);
